1fedc5ed6ca1ae756b890d38db7994334a5afdea
HugoHern <hugo8954@gmail.com>
2022-12-26 20:13:00 -0500

    added script file to start working on menu functionality and finished the css for the pop up menu

---
+++ script.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById("menu");
+const closeButton = document.getElementById("close-button");
+
+menu.addEventListener("click", () => {
+  console.log("menu clicked");
+});
+
+closeButton.addEventListener("click", () => {
+  console.log("close clicked");
+});
